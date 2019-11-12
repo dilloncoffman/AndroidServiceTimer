27e5920cd8f067f987b84f54a32acc02b6cbc2c0
Dillon Coffman <tug40394@temple.edu>
2019-11-12 16:15:55 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/service_11_12_19/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.temple.service_11_12_19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.startTimerBtn).setOnClickListener(v -> {
+
+        });
+    }
+}
---
+++ app/src/main/java/edu/temple/service_11_12_19/TimerService.java
@@ -0,0 +1,15 @@
+package edu.temple.service_11_12_19;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class TimerService extends IntentService {
+    public TimerService() {
+        super("TimerService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
