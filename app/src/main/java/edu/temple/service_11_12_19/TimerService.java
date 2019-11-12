606715b47034e28440b4547b3b93c71da742088f
Dillon Coffman <tug40394@temple.edu>
2019-11-12 16:27:35 -0500

    Broadcast to Activity from Service

--- app/src/main/java/edu/temple/service_11_12_19/TimerService.java
+++ app/src/main/java/edu/temple/service_11_12_19/TimerService.java
@@ -11,7 +11,10 @@ public class TimerService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) { // Intent that's received here is the same intent that's created to start the Service, which means you can pass extra data with it
-        int from = intent.getIntExtra("from", 20);
+        int from = intent.getIntExtra("from", 20); // cannot now update this information, IntentService means requests are independent and IntentService will queue additional results, so can't tell timer to pause
+
+        // Send a Broadcast to Activity with Service status using intent
+        Intent countdownIntent;
 
         for (int i = from; i >= 0; i--) {
             Log.d("Countdown", i + "");
@@ -20,6 +23,11 @@ public class TimerService extends IntentService {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            // Put info you want recipient Activity to get from Broadcast inside Intent
+            countdownIntent = new Intent();
+            countdownIntent.putExtra("countdown", i + "");
+            // Send Broadcast - anything broadcast this way can be received by any component on the device, not just your app
+            sendBroadcast(countdownIntent); // Context method so available in any Service or Activity
         }
     }
 }
